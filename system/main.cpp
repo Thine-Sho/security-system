d263ff9e53806575f178804322b76debb9f5f06d
Thine-Sho <Thine@Oyusha.loca>
2020-07-10 10:42:59 -0700

    updated main.cpp to be in system dir

---
+++ system/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "../cust-class/keymanage.h"
+
+class Key key;
+
+int main() {
+
+	int inputKey[key.keylen];
+	int val;
+
+	std::cout << "Keys["<<key.keylen<<"]:  ";
+	for(int i=0; i<key.keylen; ++i)
+	{
+		std::cin >> val;
+		inputKey[i] = val;
+	}
+
+    key.createKey(inputKey);
+
+    return 0;
+}
